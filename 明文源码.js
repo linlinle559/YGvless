1c4d48e6d8c07f44f0ae25b44d35b5969d4319eb
linlinle559 <linlinle559@gmail.com>
2024-12-31 00:06:15 +0800

    Update and rename vless源码.js to 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "9d9d7740-2855-472f-8ff4-56ef345d2d8d";
 
 const proxyIPs = ["\u0074\u0073\u002e\u0068\u0070\u0063\u002e\u0074\u0077"]; 
 const cn_hostnames = [''];
